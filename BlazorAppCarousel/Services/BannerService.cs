956e728732f7ea805b85c2e392ac4b4767abef2d
Rodolfo G. S. <rodolfo.gesteira@outlook.com>
2024-08-08 22:29:44 -0300

    Add project files.

---
+++ BlazorAppCarousel/Services/BannerService.cs
@@ -0,0 +1,36 @@
+﻿
+using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace BlazorAppCarousel.Services;
+
+public class BannerService : IBannerService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public BannerService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<List<string>> GetBanners()
+    {
+        var result = new List<string>();
+
+        using (var httpClient = _httpClientFactory.CreateClient())
+        {
+            var jsonString = await httpClient.GetStringAsync("https://raw.githubusercontent.com/brrj/BlazorAppCarousel/master/banners.json");
+
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                var items = JsonNode.Parse(jsonString)?.AsObject()["items"];
+
+                if (items != null)
+                    result = (JsonSerializer.Deserialize<List<string>>(items) ?? new());
+            }
+        }
+
+        return result;
+    }
+}
